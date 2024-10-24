94d8d56ffa34057a33881b43dfa1bd6a06989d13
david <kdavidmacharia8485@gmail.com>
2024-10-24 08:33:44 +0300

    added question 6

---
+++ question 6.py
@@ -0,0 +1,8 @@
+# input
+password = input("Enter the password: ")
+
+# Check if the password matches "python123"
+if password == "python123":
+    print("Access granted.")
+else:
+    print("Access denied.")
