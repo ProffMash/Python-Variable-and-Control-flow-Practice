a91e46e2867b2691fa5388bfdb4c9396418c6e18
david <kdavidmacharia8485@gmail.com>
2024-10-24 08:22:22 +0300

    added question 1 and 2

---
+++ question 1.py
@@ -0,0 +1,15 @@
+# Assign values to variables a and b
+a = 10
+b = 5
+
+# Performing the operations
+addition = a + b
+subtraction = a - b
+multiplication = a * b
+division = a / b
+
+# Printing the results
+print("Addition:", addition)
+print("Subtraction:", subtraction)
+print("Multiplication:", multiplication)
+print("Division:", division)
